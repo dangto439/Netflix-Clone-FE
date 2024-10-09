2a7acaa096318b92156298c745f3510beff88bfe
dangto <dang0898494156@gmail.com>
2024-10-09 11:40:39 +0700

    create page movies

--- src/pages/movies/index.jsx
+++ src/pages/movies/index.jsx
@@ -1,7 +1,74 @@
+import axios from "axios";
 import "./index.scss";
+import { useEffect, useState } from "react";
+import Carousel from "../../components/carousel";
+import { Col, Row } from "antd";
 
 function Movies() {
-  return <div>Movies</div>;
+  const [dataSource, setDataSource] = useState([]);
+
+  async function fetchMovies() {
+    const response = await axios.get(
+      "https://67038f65ab8a8f8927309fc3.mockapi.io/movie"
+    );
+    setDataSource(response.data);
+  }
+
+  const handleClick = (id) => {
+    console.log(id);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  return (
+    <div className="movies-layout">
+      <Carousel numberOfSlide={8} category={"Trending"} autoplay />
+      <Row>
+        <Col span={18}>
+          <div className="movies">
+            {dataSource.map((movie) => (
+              <div
+                key={movie.id}
+                className="movies__content"
+                onClick={() => handleClick(movie.id)}
+              >
+                <img src={movie.poster_path} alt="image error " />
+                <p>{movie.name}</p>
+              </div>
+            ))}
+          </div>
+        </Col>
+        <Col span={6}>
+          <div className="movies-sidebar">
+            <h1>Movie Categories</h1>
+            <ul className="categories">
+              <li>Action</li>
+              <li>Drama</li>
+              <li>Comedy</li>
+              <li>Horror</li>
+              <li>Romance</li>
+            </ul>
+
+            <h2>Popular Movies</h2>
+            <ul className="popular-movies">
+              <li>The Matrix</li>
+              <li>Inception</li>
+              <li>Interstellar</li>
+              <li>The Dark Knight</li>
+              <li>Avengers: Endgame</li>
+            </ul>
+
+            <h2>Settings</h2>
+            <ul className="settings">
+              <li>Language</li>
+            </ul>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
 export default Movies;
--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,7 +1,44 @@
+import { Col, Row } from "antd";
 import "./index.scss";
+import { Link } from "react-router-dom";
 
 function Register() {
-  return <div>Register</div>;
+  return (
+    <Row className="register">
+      <Col span={14} className="register__panel">
+        <div className="register__logo">
+          <Link to="/home">
+            <img
+              alt="image error"
+              src="https://png.pngtree.com/png-vector/20240627/ourmid/pngtree-ninja-cartoon-logo-vector-png-image_12858146.png"
+              width={200}
+            />
+          </Link>
+          <h1>Welcome to my page -- create by Dang To</h1>
+        </div>
+        <iframe
+          className="login__video"
+          src="https://www.youtube.com/embed/-iFq6IcAxBc?si=RlmhO40aGN9lYTcY&amp;autoplay=1&mute=1"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+      </Col>
+      <Col span={10} className="register__content">
+        <h3>Register new account</h3>
+        <input type="text" placeholder="Full Name" />
+        <input type="text" placeholder="Username" />
+        <input type="number" placeholder="Phone" />
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button>Register</button>
+        <div className="register__redirect">
+          <Link to="/login">Login</Link>
+          <Link to="/forgot-password">Forgot password</Link>
+        </div>
+      </Col>
+    </Row>
+  );
 }
 
 export default Register;
